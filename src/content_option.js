59e706db5603d1381b31fcd079a92e1a31a8a7f1
= <=>
2023-11-01 00:40:38 -0700

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,153 @@
+import blocktopia from "./assets/images/blocktopia.jpg"
+import calculator from "./assets/images/calculator.jpg"
+import crud from "./assets/images/crud.jpg"
+import stopwatch from "./assets/images/stopwatch.jpg"
+import ticimage from "./assets/images/ticimage.jpg"
+
+const logotext = "Hasnain Ansari";
+const meta = {
+    title: "Hasnain Ansari",
+    description: "I’m Hasnain Ansari Full stack devloper.",
+};
+
+const introdata = {
+    title: "I’m Hasnain Ansari",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop fast code",
+    },
+    description: "Additionally, I am currently as a software developer and AutoElectrician in EFI VEHICLE",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "react",
+        value: 90,
+    },
+    {
+        name: "HTML,CSS",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "BEST UI DESIGN IN USER EXPERIENCE",
+    },
+    {
+        title: "WEB Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: blocktopia,
+        description: "Blocktopia user experience UI website",
+        link: "https://hasnainansari12.github.io/blocktopia/",
+    },
+    {
+        img: calculator,
+        description: "Personal reactjs calculator with user Experience",
+        link: "https://reactjs-calculator-3hzw.vercel.app/",
+    },
+    {
+        img: crud,
+        description: "Personal reactjs CRUD OPERATION with user Experience.",
+        link: "https://react-with-todo-ptot1czwl-hasnainansari12.vercel.app",
+    },
+
+    {
+        img: ticimage,
+        description: "Personal reactjs CRUD tic-tac-toe game with user Experience.",
+        link: "https://hasnainansari12.github.io/tic.tac.toe/",
+    },
+    {
+        img: stopwatch,
+        description: "Personal reactjs CRUD stopo watch with user Experience.",
+        link: "https://hasnainansari12.github.io/STOPWATCH/",
+    },
+   
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+  ,
+ 
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "",
+    YOUR_FONE: "03241454261",
+    description: " Additionally, I am currently as a software developer  and AutoElectrician in EFI vehicle", 
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
